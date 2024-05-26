0b77b4b4353e656684b5e73ea183ee5d63358503
Quang <quang.ha@QrM3P.local>
2024-05-26 00:01:00 -0400

    copy project rebuild

---
+++ Rebuild/Packages/TheMovieDBAPI/Sources/TheMovieDBAPI/ImageQuality.swift
@@ -0,0 +1,3 @@
+public enum TMDBImageQuality: String {
+    case w500, w400, w300, w100, original
+}
---
+++ Rebuild/Rebuild/RebuildApp.swift
@@ -0,0 +1,17 @@
+//
+//  RebuildApp.swift
+//  Rebuild
+//
+//  Created by Quang on 2024-04-18.
+//
+
+import SwiftUI
+
+@main
+struct RebuildApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
