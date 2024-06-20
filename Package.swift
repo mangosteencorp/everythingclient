b0747cb0580c8c7bec769e9df603b92348c4f61a
Quang <quang.ha@QrM3P.local>
2024-06-19 23:01:40 -0400

    restructing the libraries

--- Package.swift
+++ Package.swift
@@ -19,10 +19,12 @@ let package = Package(
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "everythingclient"),
+            name: "everythingclient", dependencies: ["TMDB"]),
         .testTarget(
             name: "everythingclientTests",
             dependencies: ["everythingclient"]),
         
+        .target(name: "TMDB", dependencies: ["TMDB.Dimilian.MVVM"]),
+        .target(name: "TMDB.Dimilian.MVVM"),
     ]
 )
