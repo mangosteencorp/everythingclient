6632b9db937ea41b8344f5040d7b0353f00bbf1e
Quang <quang.ha@QrM3P.local>
2024-06-21 11:30:26 -0400

    wrap it with controller doesn't work

--- Sources/everythingclient/TMDBAPITabView.swift
+++ Sources/everythingclient/TMDBAPITabView.swift
@@ -1,22 +1,15 @@
-//
-//  SwiftUIView.swift
-//  
-//
-//  Created by Quang on 2024-06-09.
-//
-
 import SwiftUI
 
+@available(iOS 15, *)
 struct TMDBAPITabView: View {
-    @State private var selectionTab: Int = 0
+    // @State private var selectionTab: Int = 0
+    let nowplayingView: DMSNowPlayingView = DMSNowPlayingView()
     var body: some View {
-        TabView(selection: $selectionTab,
-                content:  {
-            if #available(iOS 15, *) {
-                DMSNowPlayingView().tabItem { /*@START_MENU_TOKEN@*/Text("Tab Label 1")/*@END_MENU_TOKEN@*/ }.tag(1)
-            } 
-            Text("Tab Content 2").tabItem { /*@START_MENU_TOKEN@*/Text("Tab Label 2")/*@END_MENU_TOKEN@*/ }.tag(2)
-        })
+        TabView  {
+            NowPlayingViewControllerRepresentable()
+            nowplayingView.tag(1)
+            Text("Tab Content 2").tag(2)
+        }
         .tabViewStyle(PageTabViewStyle())
         .indexViewStyle(PageIndexViewStyle(backgroundDisplayMode: .always))
     }
