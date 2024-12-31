87e7a948c9bffb4f6c7a14067b7c81789267c607
Quang <quang.ha@Mac.lan>
2024-12-31 18:09:36 -0500

    2 extra views

---
+++ Sources/TMDB_Shared_UI/Views/ErrorViews/ServerErrorView.swift
@@ -0,0 +1,38 @@
+import SwiftUI
+
+struct ServerErrorView: View {
+    var retryAction: () -> Void
+    
+    var body: some View {
+        VStack(spacing: 16) {
+            Image(systemName: "exclamationmark.triangle.fill")
+                .font(.system(size: 70))
+                .foregroundColor(.orange)
+            
+            Text("Oops! Something Went Wrong")
+                .font(.title2)
+                .fontWeight(.bold)
+            
+            Text("We're having trouble connecting to our servers.\nPlease try again later.")
+                .font(.body)
+                .multilineTextAlignment(.center)
+                .foregroundColor(.gray)
+                .padding(.horizontal)
+            
+            Button(action: retryAction) {
+                Text("Try Again")
+                    .fontWeight(.semibold)
+                    .foregroundColor(.white)
+                    .frame(height: 50)
+                    .frame(maxWidth: .infinity)
+                    .background(Color.blue)
+                    .cornerRadius(10)
+            }
+            .padding(.horizontal, 30)
+            .padding(.top, 10)
+        }
+    }
+} 
+#Preview{
+    ServerErrorView(retryAction: {})
+}
