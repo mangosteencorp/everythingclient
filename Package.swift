365fbfb7579979bc13eeb13d532b15fd01d13380
Quang <quang.ha@QrM3P.local>
2025-01-17 09:54:26 -0500

    config json that works

--- Package.swift
+++ Package.swift
@@ -43,6 +43,10 @@ let package = Package(
         .package(url: "https://github.com/kean/Nuke.git", from: "12.0.0"),
         .package(url: "https://github.com/onevcat/Kingfisher.git", from: "8.0.0"),
         .package(url: "https://github.com/nalexn/ViewInspector", from: "0.10.0"),
+        .package(
+            url: "https://github.com/apollographql/apollo-ios.git",
+            .upToNextMajor(from: "1.0.0")
+        ),
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
