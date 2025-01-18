1ad138c40d6e94a06522d31a0078f4f712dc88b5
Quang <quang.ha@QrM3P.local>
2025-01-17 22:19:24 -0500

    generated Swift (to be removed)

---
+++ Sources/GraphQLAPI/API/Sources/Schema/SchemaMetadata.graphql.swift
@@ -0,0 +1,33 @@
+// @generated
+// This file was automatically generated and should not be edited.
+
+import ApolloAPI
+
+public protocol SelectionSet: ApolloAPI.SelectionSet & ApolloAPI.RootSelectionSet
+where Schema == API.SchemaMetadata {}
+
+public protocol InlineFragment: ApolloAPI.SelectionSet & ApolloAPI.InlineFragment
+where Schema == API.SchemaMetadata {}
+
+public protocol MutableSelectionSet: ApolloAPI.MutableRootSelectionSet
+where Schema == API.SchemaMetadata {}
+
+public protocol MutableInlineFragment: ApolloAPI.MutableSelectionSet & ApolloAPI.InlineFragment
+where Schema == API.SchemaMetadata {}
+
+public enum SchemaMetadata: ApolloAPI.SchemaMetadata {
+  public static let configuration: any ApolloAPI.SchemaConfiguration.Type = SchemaConfiguration.self
+
+  public static func objectType(forTypename typename: String) -> ApolloAPI.Object? {
+    switch typename {
+    case "pokemon_v2_pokemon": return API.Objects.Pokemon_v2_pokemon
+    case "pokemon_v2_pokemonsprites": return API.Objects.Pokemon_v2_pokemonsprites
+    case "query_root": return API.Objects.Query_root
+    default: return nil
+    }
+  }
+}
+
+public enum Objects {}
+public enum Interfaces {}
+public enum Unions {}
