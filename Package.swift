04f81e9c64b06de2a65abc959656f8fa29630e19
Quang <quang.ha@QrM3P.local>
2025-01-19 22:47:46 -0500

    remove unused 3rd party

--- Package.swift
+++ Package.swift
@@ -42,8 +42,7 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/Swinject/Swinject.git", from: "2.8.0"),
-        .package(url: "https://github.com/kishikawakatsumi/KeychainAccess.git", from: "4.2.2"),
-        .package(url: "https://github.com/kean/Nuke.git", from: "12.0.0"),
+        
         .package(url: "https://github.com/onevcat/Kingfisher.git", from: "8.0.0"),
         .package(url: "https://github.com/nalexn/ViewInspector", from: "0.10.0"),
         .package(
