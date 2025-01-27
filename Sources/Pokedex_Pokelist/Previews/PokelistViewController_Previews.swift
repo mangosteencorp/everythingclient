59319f729f61108eef0eb78a8af9b93211eaa370
Quang <quang.ha@QrM3P.local>
2025-01-27 18:03:35 -0500

    fixing preview for this

--- Sources/Pokedex_Pokelist/Previews/PokelistViewController_Previews.swift
+++ Sources/Pokedex_Pokelist/Previews/PokelistViewController_Previews.swift
@@ -4,10 +4,26 @@ import Shared_UI_Support
 
 #if DEBUG
 let pokemonService = PokemonService.shared
-//let previewViewController = PokelistRouter.createModule(pokemonService: pokemonService)
+class ExamplePokelistRouter: PokelistRouterProtocol {
+    static func createModule(pokemonService: Pokedex_Shared_Backend.PokemonService) -> UIViewController {
+        let presenter = PokelistPresenter()
+        let interactor = PokelistInteractor(pokemonService: pokemonService)
+        let router = ExamplePokelistRouter()
+        let view = PokelistViewController()
+        view.presenter = presenter
+        presenter.view = view
+        presenter.router = router
+        presenter.interactor = interactor
+        interactor.presenter = presenter
+        return view
+    }
+    
+    func navigateToPokemonDetail(from view: UIViewController?, with id: Int) {}
+}
+
 #Preview {
     UIViewControllerPreview {
-        UIViewController()
+        ExamplePokelistRouter.createModule(pokemonService: pokemonService)
     }
 }
 #endif
