5cb32518cd11b7eca62f869b751ca25710934320
Quang <quang.ha@QrM3P.local>
2025-03-08 00:37:10 -0500

    this switching theme is really nice

--- Sources/CoreFeatures/ThemeSwitcher.swift
+++ Sources/CoreFeatures/ThemeSwitcher.swift
@@ -1,25 +1,16 @@
 import SwiftUI
 
-public extension ThemeManager {
-    func switchToNextTheme() {
-        let availableThemes = availableThemes()
-        guard let currentIndex = availableThemes.firstIndex(where: { $0.backgroundColor == currentTheme.backgroundColor }) else { return }
-        let nextIndex = (currentIndex + 1) % availableThemes.count
-        setTheme(availableThemes[nextIndex])
-    }
-}
-
 public struct ThemeSwitchButton: View {
     @EnvironmentObject var themeManager: ThemeManager
-    
+
     public init() {}
-    
+
     public var body: some View {
         Button(action: {
             themeManager.switchToNextTheme()
-        }) {
+        }, label: {
             Image(systemName: "arrow.trianglehead.swap")
-                .foregroundColor(themeManager.currentTheme.labelColor)
-        }
+                .foregroundColor(.primary)
+        })
     }
-} 
+}
